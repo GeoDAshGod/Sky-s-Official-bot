fcf0dc2b4ade30df7701f1ffd6c3a1fe1d82cdf3
GeoDAshGod <42033709+GeoDAshGod@users.noreply.github.com>
2019-09-02 12:53:36 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -16,7 +16,7 @@ async def on_ready():
 
 
 @client.command(pass_context=True)
-async def play (ctx):
+async def play (ctx, url):
     channel = ctx.message.author.voice_channel
     await client.join_voice_vhannel(channel)
     server = ctx.message.server
